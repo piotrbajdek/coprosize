d21df726b05f403925a46bafb3193a2bdb080c6a
Piotr Bajdek <96956242+piotrbajdek@users.noreply.github.com>
2022-06-01 04:37:14 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-UNDER CONSTRUCTION
+# OVERVIEW OF LNGCNV
+
+[coprosize](https://github.com/piotrbajdek/coprosize) employs power and cubic regression models allowing to estimate the producer's body mass based on coprolite diameter. Models can be chosen accordingly to the supposed producer's taxon (at this stage of program development, only tetrapod models are implemented) and its diet type (carnivorous, herbivorous, omnivorous, unspecified). Implemented regression formulae are provided in [Supplement 1. Regression models](https://github.com/piotrbajdek/coprosize/blob/main/docs/supplement-1.ods) and constructed based on the data given in [Supplement 2. Scat diameters and body masses](https://github.com/piotrbajdek/coprosize/blob/main/docs/supplement-2.ods).
+
+As it is aimed for science, [coprosize](https://github.com/piotrbajdek/coprosize) is written in Rust. The reasons for this choice are (1) the high code correctness guaranteed by Rust, (2) to ensure that each program version will be hosted 'for perpetuity' in the [registry](https://docs.rs/crate/coprosize/latest) and (3) that each program version will remain easily installable and cross-platform 'for perpetuity', thanks to the Rust's strict policy of backward compatibility.
+
+**As of v1.0.0-alpha.1, coprosize remains in an unstable fast-development phase. This program version is not intended for scientific research but for presentation of the technology and testing!**
+
+# SCREENSHOTS
+
+[Static links to changeable images of the _most recent_ version of coprosize! This may include pre-releases!]
+
+![help-image](https://github.com/piotrbajdek/coprosize/blob/main/docs/images/help-image.png?raw=true)
+
+![example-image-1](https://github.com/piotrbajdek/coprosize/blob/main/docs/images/example-image-1.png?raw=true)
+
+# CITATION AND REUSE
+
+Please, always refer to a specific program version--implemented formulae are subject to change if new data are available (or simply studied by the author) or bugs of any kind are detected. Although coprosize is designed with the needs of a user in mind, you are perfectly OK to reuse my models in your study without really installing coprosize as long as you cite this computer program as the original source. You are also OK to modify and fork coprosize under terms of the [MIT license](https://github.com/piotrbajdek/coprosize/blob/main/LICENSE).
+
+Bajdek, P., 2022. coprosize (version 1.0.0-alpha.1). [computer software] https://github.com/piotrbajdek/coprosize
+
+# INSTALLATION ON LINUX
+
+[coprosize](https://github.com/piotrbajdek/coprosize) should run smoothly on Windows and macOS. Yet, it is being developed and tested on Fedora Linux.
+
+## METHOD 1
+
+**1.** Install from crates.io by the use of cargo:
+
+_cargo install coprosize \--version 1.0.0-alpha.1_
+
+By default, the file will be downloaded to .cargo/bin/, a hidden folder in your home directory.
+
+**2a.** For convenience, you will probably want to copy lngcnv to /usr/bin/ as in Method 2 (3a, 3b).
+
+**2b.** Alternatively, add ~/.cargo/bin directory to your PATH variable (see documentation of your shell).
+
+## METHOD 2
+
+**1.** Download the binary 'coprosize' for Linux x86_64 from GitHub:
+
+https://github.com/piotrbajdek/coprosize/releases/tag/v1.0.0-alpha.1
+
+**2.** Make the file executable:
+
+_sudo chmod +x ./coprosize_
+
+**3a.** Install coprosize via copying the binary to /usr/bin/
+
+_sudo cp coprosize /usr/bin/_
+
+**3b.** On Fedora Silverblue / Kinoite:
+
+_sudo cp coprosize /var/usrlocal/bin/_
+
+## METHOD 3
+
+Download the coprosize source from GitHub. Then, build and install the program:
+
+https://github.com/piotrbajdek/coprosize/releases/tag/v1.0.0-alpha.1
+
+_cargo build && sudo cp target/debug/coprosize /usr/bin/_
+
+# COPROSIZE CRATE ON CRATES.IO
+
+The Rust community’s crate registry
+
+https://crates.io/crates/coprosize
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,183 @@
+// COPROSIZE VERSION 1.0.0-ALPHA.1 / THE MIT LICENSE (MIT) © 2022 PIOTR BAJDEK
+
+use std::env;
+
 fn main() {
+
+   let clear = "\x1b[0m"; // reset the font color
+   let red = "\x1b[31m";
+   let bright_yellow = "\x1b[93m";
+   let blue_underlined = "\x1b[34;4m";
+   let cyan = "\x1b[36m";
+   let grey = "\x1b[38;5;240m";
+
+// ARGUMENTS ANYWHERE WITHIN THE STRING
+
+   for argument in env::args() {
+
+// ABOUT
+
+      if argument == "-a" || argument == "--about" {
+      println!("Program:  coprosize");
+      println!("Version:  1.0.0-alpha.1");
+      println!("Date:     June 01, 2022");
+      println!("Author:   Piotr Bajdek");
+      println!("Contact:  {}", blue_underlined.to_owned() + "piotr.bajdek@proton.me" + clear);
+      println!("ORCID:    {}", blue_underlined.to_owned() + "https://orcid.org/0000-0003-2678-3122" + clear);
+      println!("Source:   {}", blue_underlined.to_owned() + "https://github.com/piotrbajdek/coprosize" + clear);
+      println!("License:  The MIT License (MIT) © 2022 Piotr Bajdek");
+      return;
+      }
+
+// CHANGES
+
+      if argument == "-c" || argument == "--changes" {
+      println!("--.--.---- v1.0.0 – Implemented --carnivorous --amphibia, --carnivorous --archosauria, --carnivorous --mammalia, --carnivorous --squamata, --herbivorous --archosauria, --herbivorous --mammalia, --omnivorous --archosauria, --omnivorous --mammalia, --unspecified --testudines");
+      return;
+      }
+
+// CITATION
+
+      if argument == "-C" || argument == "--citation" {
+      println!("Bajdek, P., 2022. coprosize (version 1.0.0-alpha.1). [computer software] https://github.com/piotrbajdek/coprosize");
+      return;
+      }
+
+// HELP
+
+      if argument == "-h" || argument == "--help" {
+      println!("Usage:{}", bright_yellow.to_owned() + "    coprosize [DIET / TAXON] [TAXON / DIET] [COPROLITE DIAMETER IN MM]");
+      println!("{}", clear);
+      println!("          {}", cyan.to_owned() + "--carnivorous --amphibia" + clear + "          Model for carnivorous amphibians");
+      println!("          {}", cyan.to_owned() + "--carnivorous --archosauria" + clear + "       Model for carnivorous archosaurs");
+      println!("          {}", grey.to_owned() + "--carnivorous --mammalia" + clear + "          Model for carnivorous mammalians");
+      println!("          {}", cyan.to_owned() + "--carnivorous --squamata" + clear + "          Model for carnivorous squamates");
+      println!("          {}", grey.to_owned() + "--herbivorous --archosauria" + clear + "       Model for herbivorous archosaurs");
+      println!("          {}", grey.to_owned() + "--herbivorous --mammalia" + clear + "          Model for herbivorous mammalians");
+      println!("          {}", grey.to_owned() + "--omnivorous --archosauria" + clear + "        Model for omnivorous archosaurs");
+      println!("          {}", grey.to_owned() + "--omnivorous --mammalia" + clear + "           Model for omnivorous mammalians");
+      println!("          {}", cyan.to_owned() + "--unspecified --testudines" + clear + "        Model for turtles of all diets");
+      println!("{}", grey);
+      println!("            Features in grey are not yet implemented during the alpha phase!");
+      println!("{}", clear);
+      println!("Examples:{}", bright_yellow.to_owned() + " coprosize --carnivorous --archosauria 27.75" + clear + " [diameter given in mm]");
+      println!("{}", bright_yellow.to_owned() + "          coprosize --archosauria --carnivorous 27.75" + clear + " [diameter given in mm]");
+      println!("");
+      println!("See also: {}", cyan.to_owned() + "-a" + clear + ", " + cyan + "--about" + clear + "     Show contact and program info");
+      println!("          {}", cyan.to_owned() + "-c" + clear + ", " + cyan + "--changes" + clear + "   Show simplified change notes");
+      println!("          {}", cyan.to_owned() + "-C" + clear + ", " + cyan + "--citation" + clear + "  Show how to cite this program");
+      println!("          {}", cyan.to_owned() + "-h" + clear + ", " + cyan + "--help" + clear + "      Show this help");
+      println!("          {}", cyan.to_owned() + "-l" + clear + ", " + cyan + "--license" + clear + "   Show licesing information");
+      println!("          {}", cyan.to_owned() + "-v" + clear + ", " + cyan + "--version" + clear + "   Show the program version");
+      return;
+      }
+
+// LICENSE
+
+      if argument == "-l" || argument == "--license" {
+      println!("{}", bright_yellow.to_owned() + "The MIT License (MIT)" + clear);
+      println!("");
+      println!("Copyright © 2022 Piotr Bajdek");
+      println!("");
+      println!("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:");
+      println!("");
+      println!("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.");
+      println!("");
+      println!("THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
+      return;
+      }
+
+// VERSION
+
+      if argument == "-v" || argument == "--version" {
+      println!("Version: 1.0.0-alpha.1");
+      println!("June 01, 2022");
+      return;
+      }
+   }
+
+// COLLECT ARGUMENTS FOR CALCULATIONS
+
+   let args: Vec<String> = env::args().collect();
+
+   let input1 = args.get(1).expect(&(red.to_owned() + "Invalid or missing arguments! See: --help" + clear));
+   let input2 = args.get(2).expect(&(red.to_owned() + "Invalid or missing arguments! See: --help" + clear));
+
+// CARNIVOROUS AMPHIBIA
+
+   if input1 == "--carnivorous" && input2 == "--amphibia" || input1 == "--amphibia" && input2 == "--carnivorous" {
+   let diameter = args.get(3).expect(&(red.to_owned() + "No diameter inserted! See: --help" + clear));
+   let dcal: f32 = diameter.parse().expect(&(red.to_owned() + "Incorrect diameter value! Program only processes numbers!" + clear));
+
+   let power1 = f32::powi(dcal as f32, 3);
+   let power2 = f32::powi(dcal as f32, 2);
+   let mass = (0.0004064349 * power1) - (0.0041616775 * power2) + (0.0147514015 * dcal) - 0.0122201640;
+
+   println!("Coprolite diameter: {}", bright_yellow.to_owned() + diameter + " mm" + clear);
+   print!("Producer's body mass: {}", bright_yellow.to_owned());
+   print!("{:.3}", mass);
+   print!(" kg{}", clear.to_owned());
+   println!(" = 0.0004064349 * {}", diameter.to_owned() + "³ - 0.0041616775 * " + diameter + "² + 0.0147514015 * " + diameter + " - 0.0122201640");
+   println!("Regression model based on:{}", bright_yellow.to_owned() + " extant carnivorous batrachians");
+   return;
+   }
+
+// CARNIVOROUS ARCHOSAURIA
+
+   if input1 == "--carnivorous" && input2 == "--archosauria" || input1 == "--archosauria" && input2 == "--carnivorous" {
+   let diameter = args.get(3).expect(&(red.to_owned() + "No diameter inserted! See: --help" + clear));
+   let dcal: f32 = diameter.parse().expect(&(red.to_owned() + "Incorrect diameter value! Program only processes numbers!" + clear));
+
+   let power = f32::powf(dcal as f32, 2.5928826261);
+   let mass = 0.0023303585 * power;
+
+   println!("Coprolite diameter: {}", bright_yellow.to_owned() + diameter + " mm" + clear);
+   print!("Producer's body mass: {}", bright_yellow.to_owned());
+   print!("{:.3}", mass);
+   print!(" kg{}", clear.to_owned());
+   println!(" = 0.0023303585 * {}", diameter.to_owned() + "²·⁵⁹²⁸⁸²⁶²⁶¹");
+   println!("Regression model based on:{}", bright_yellow.to_owned() + " extant carnivorous birds and crocodilians");
+   return;
+   }
+
+// CARNIVOROUS SQUAMATA
+
+   if input1 == "--carnivorous" && input2 == "--squamata" || input1 == "--squamata" && input2 == "--carnivorous" {
+   let diameter = args.get(3).expect(&(red.to_owned() + "No diameter inserted! See: --help" + clear));
+   let dcal: f32 = diameter.parse().expect(&(red.to_owned() + "Incorrect diameter value! Program only processes numbers!" + clear));
+
+   let power = f32::powf(dcal as f32, 2.2890083731);
+   let mass = 0.0005658348 * power;
+
+   println!("Coprolite diameter: {}", bright_yellow.to_owned() + diameter + " mm" + clear);
+   print!("Producer's body mass: {}", bright_yellow.to_owned());
+   print!("{:.3}", mass);
+   print!(" kg{}", clear.to_owned());
+   println!(" = 0.0005658348 * {}", diameter.to_owned() + "²·²⁸⁹⁰⁰⁸³⁷³¹");
+   println!("Regression model based on:{}", bright_yellow.to_owned() + " extant carnivorous squamates");
+   return;
+   }
+
+// UNSPECIFIED TESTUDINES
+
+   if input1 == "--unspecified" && input2 == "--testudines" || input1 == "--testudines" && input2 == "--unspecified" {
+   let diameter = args.get(3).expect(&(red.to_owned() + "No diameter inserted! See: --help" + clear));
+   let dcal: f32 = diameter.parse().expect(&(red.to_owned() + "Incorrect diameter value! Program only processes numbers!" + clear));
+
+   let power = f32::powf(dcal as f32, 2.1345692041);
+   let mass = 0.0251303814 * power;
+
+   println!("Coprolite diameter: {}", bright_yellow.to_owned() + diameter + " mm" + clear);
+   print!("Producer's body mass: {}", bright_yellow.to_owned());
+   print!("{:.3}", mass);
+   print!(" kg{}", clear.to_owned());
+   println!(" = 0.0251303814 * {}", diameter.to_owned() + "²·¹³⁴⁵⁶⁹²⁰⁴¹");
+   println!("Regression model based on:{}", bright_yellow.to_owned() + " extant carnivorous, omnivorous and herbivorous turtles");
+   return;
+   }
+
+// INVALID ARGUMENT [1] AND/OR [2]
+
+   panic!("{}", red.to_owned() + "Invalid arguments provided! See: --help" + clear);
 
 }
